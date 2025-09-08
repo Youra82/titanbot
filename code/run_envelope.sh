83a9870be93e67cfc2f4862ef7b5a30ec879b2f6
Youra82 <matola@outlook.de>
2025-09-08 22:16:33 +0200

    Update run_envelope.sh

--- code/run_envelope.sh
+++ code/run_envelope.sh
@@ -1,3 +1,3 @@
 # Passe den Pfad an dein System an, falls nötig
-source /home/ubuntu/LiveTradingBots/code/.venv/bin/activate
-python3 /home/ubuntu/LiveTradingBots/code/strategies/envelope/run.py
+source /home/ubuntu/titanbot/code/.venv/bin/activate
+python3 /home/ubuntu/titanbot/code/strategies/envelope/run.py
