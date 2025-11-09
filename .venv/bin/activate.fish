29a9eca7d9fa0b4cfc4e7b9f0a3319396de96076
Youra82 <matola@outlook.de>
2025-11-09 20:40:27 +0100

    Rollback auf stabile Server-Version vom 12.10.

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /root/titanbot/.venv
+set -gx VIRTUAL_ENV /home/matola/titanbot/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
